cd0e930d6d191193e781378e91f496198a5960fa
Ethan Chan <ethchan@deloitte.ca>
2018-08-09 15:32:13 -0400

    not working yet

--- app.js
+++ app.js
@@ -3,20 +3,47 @@ const app = express();
 const path = require('path');
 const graphqlHTTP = require('express-graphql');
 const jwt = require('jsonwebtoken');
-
-const router = express.Router();
-
 //Custom Scripts and functions
 const schema = require('./GraphqlSchema/index');
 const auth = require('./authHandler');
 
 app.use(express.json());
+
 app.use(express.urlencoded({extended:true}));
-app.disable('x-powered-by')
+// app.disable('x-powered-by');
+
+app.listen(8080, function() {
+    console.log("purchaseInsights server has started on port 8080");
+});
+const router = express.Router();
 
 router.get('/', function(req,res){
 	res.json({
         text: 'Hello World'
+    });
+});
+
+router.get('/logAccess', function(req, res){
+    res.sendFile(path.join(__dirname, 'token.html'));    
+});
+
+// router.get('/getToken', function(req, res){
+//     console.log(req.body);
+//     var user = req.body.Username;
+//     var pwd = req.body.pwd;
+//     res.json({
+//         user: user,
+//         password: pwd
+//     });
+//     // user = {
+//     //     id: user,  
+//     // }
+// });
+router.post('/signIn', function(req, res){
+    console.log(req.body);
+    res.json({
+        user: req.body.Username,
+        password: req.body.pwd
     });
 });
 
@@ -30,8 +57,3 @@ app.use('/graphql', graphqlHTTP({
 	schema: schema,
 	graphiql: true
 }));
-
-
-app.listen(8080, function() {
-    console.log("purchaseInsights server has started on port 8080");
-});
